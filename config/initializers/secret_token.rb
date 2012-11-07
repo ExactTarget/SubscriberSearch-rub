e5c2e9ef297165362e9eb8a22829682ed49bca7d
Kelly Andrews <kandrews@exacttarget.com>
2012-11-07 09:46:36 -0500

    V 0
    Initial RoR install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SubscribersearchRub::Application.config.secret_token = '1c83762ab20492f93d43223f268d20384d959725c05d3702b9f16106fbb13d35192df452dcf195ed5a5a6e33b56476ce1bc4eec375783737c2144b236e153bbb'
