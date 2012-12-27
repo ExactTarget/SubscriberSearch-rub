dd49f0b9cfb9af7e2ba0b1132dcf8a289ba550f4
Kelly Andrews <kandrews@exacttarget.com>
2012-12-27 09:49:31 -0500

    Updated files for step3 complete

--- routes/main.rb
+++ routes/main.rb
@@ -5,6 +5,11 @@ class SubscriberSearch < Sinatra::Application
 #Main route to show application. This gets the SOAP route, and displays the main template.
 
 	get "/" do
+		p settings.oauthToken
+		p settings.internalOauthToken
+		p settings.refreshToken
+		p settings.jwt
+		p settings.exp
 		get_soap_route
 		@title = "Subscriber Search"
 		slim :main
@@ -22,6 +27,20 @@ class SubscriberSearch < Sinatra::Application
 	post "/logout" do
 
 	end
-		
+
+# sidebar runs the sidebar template code in the Main template.	
+	def sidebar
+		@SidebarTitle = "Subscriber Details"
+		slim :sidebar, :layout => false
+	end
+
+# grid runs the grid template inside the main template.
+	
+	def grid
+		@gridTitle = "Subscriber Listings"
+		@searchPlaceholder = "Search for Subscribers"
+		slim :grid, :layout => false
+	end
+	
 
 end
